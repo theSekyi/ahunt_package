5c27553c8496db1918085c48630db4c4d6d37b5f
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-09-03 18:38:59 +0200

    setup init file

--- ahunt_package/__init__.py
+++ ahunt_package/__init__.py
@@ -1,5 +1,10 @@
 """Top-level package for ahunt-package."""
 
+from arch import *
+from train import *
+from visualization import *
+from utils import *
+
 __author__ = """Emmanuel K. Sekyi"""
-__email__ = 'emmanuelkwamesekyi@gmail.com'
-__version__ = '0.1.0'
+__email__ = "emmanuelkwamesekyi@gmail.com"
+__version__ = "0.1.0"
